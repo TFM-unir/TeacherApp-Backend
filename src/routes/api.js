1472d9cb3ea550f87b22077c501f273e7b5e4e9a
miquelmb <miquelmayordomo@gmail.com>
2023-12-11 17:16:43 +0100

    Cambios para funcionamiento admin-control-panel

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,7 @@ const { checkToken } = require('../middlewares/auth.middleware');
 const router = require('express').Router();
 
 // Rutas
-//TODO: hay que ver mejor esto, por que tenemos que bloquear las ruas que no vayan a ser publicas y no s epuede sin definir cual es la publica.
+//TODO: hay que ver mejor esto, por que tenemos que bloquear las ruas que no vayan a ser publicas y no se puede sin definir cual es la publica.
 router.use('/teachers', require('./api/teachers'));
 router.use('/students', checkToken, require('./api/students'));
 router.use('/users', require('./api/users'));
